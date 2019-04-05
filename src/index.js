cede9b4aa828e75a7c55f1b0d8b777516f8deb5f
dima <dmitriy.dubchenko@gmail.com>
2019-04-05 03:35:23 +0200

    innitial

--- src/index.js
+++ src/index.js
@@ -1,12 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+class HelloString extends React.Component {
+	render(props) {
+		return (
+			<div className="querry-string">
+				Hello {this.props.name} !
+			</div>
+		)
+	}
+}
+
+class SayHello extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: "Stranger"
+		}
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(e) {
+		this.setState({
+			name: e.target.value
+		})
+		console.log(this.state.name);
+	}
+
+	render() {
+		var heroName = (this.state.name.length > 0) ? this.state.name : "Stranger";
+		return (
+			<div className="hello-string">
+				<input type='text' onChange={this.handleChange} />
+				<div>
+					<HelloString
+						name={heroName}
+					/>
+
+				</div>
+			</div>
+		)
+	}
+
+
+}
+
+
+ReactDOM.render(<SayHello />, document.getElementById('root'));
